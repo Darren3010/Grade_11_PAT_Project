656d1d2305086498107e7df4e7f3fe08d6d6c88a
Darren <85887153+Darren3010@users.noreply.github.com>
2021-08-28 13:56:16 +0200

    Update Merc300SL_u.pas

--- Code/Merc300SL_u.pas
+++ Code/Merc300SL_u.pas
@@ -74,9 +74,9 @@ VAR
 begin
   RedOutput.Lines.Clear;
   RedOutput.Visible := True;
-  if fileexists('300slBio.txt') then
+  if fileexists('Textfile\300slBio.txt') then
   begin
-    assignfile(TFile, '300slBio.txt');
+    assignfile(TFile, 'Textfile\300slBio.txt');
     reset(TFile);
     while not eof(TFile) do
     begin
@@ -94,9 +94,9 @@ begin
   RedOutput.ScrollBars := ssVertical;
   RedOutput.Lines.Clear;
   RedOutput.Visible := True;
-  if fileexists('Directions.txt') then
+  if fileexists('Textfile\Directions.txt') then
   begin
-    assignfile(TFile, 'Directions.txt');
+    assignfile(TFile, 'Textfile\Directions.txt');
     reset(TFile);
     while not eof(TFile) do
     begin
